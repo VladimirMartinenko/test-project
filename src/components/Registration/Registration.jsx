63cab38a165b38984f6a2de2b16f9234833a4614
Vladimir Martinenko <teslabest87@gmail.com>
2024-02-08 15:07:16 +0100

    add error

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -26,8 +26,8 @@ const Registration = ({ onChange }) => {
   }
 
   const [position, setPosition] = useState(null)
-  const [token, setToken] = useState(null)
   const [user, setUser] = useState(null)
+  const [error, seError] = useState(null)
 
   // get position from radio button
   useEffect(() => {
@@ -79,102 +79,31 @@ const Registration = ({ onChange }) => {
             console.log(data)
             if (data.success) {
               console.log('process success response')
-              handleChange()
+              handleChange();
+              utils.resetForm()
             } else {
               console.log('proccess server errors')
+              seError(data.message)
             }
           })
           .catch(error => console.log(error))
       )
       .catch(error => console.log(error))
 
-    utils.resetForm()
+    // utils.resetForm()
   }
 
   return (
     <section className={style.main} id='registration'>
-      {/* <h1 className={style.h1}>Working with POST request</h1> */}
-      {/* <Formik
-        initialValues={initialValues}
-        validationSchema={REGISTRATION_CHEMA}
-        onSubmit={onSubmit}
-      >
-        {({ values, errors, field, meta, touched, isValid, dirty }) => {
-          const buttonStyles = cx(style.button, {
-            [style.validButton]: dirty && isValid
-          })
-          return (
-            <Form className={style.form}>
-              <Input name='name' type='text' placeholder='Your name' minlength="2" maxlength="60"/>
-              <Input name='email' type='email' placeholder='Email' minlength="2" maxlength="100"/>
-              <Input
-                name='phone'
-                type='tel'
-                placeholder='Phone'
-                text='+38 (XXX) XXX - XX - XX'
-                maxlength="13"
-              />
-              <div id='my-radio-group' className={style.h1_radio}>
-                Select your position
-              </div>
-
-              <div
-                className={style.radio_position}
-                role='group'
-                aria-labelledby='my-radio-group'
-              >
-                {position &&
-                  position.positions.map(position => (
-                    <div key={position.id}>
-                      <label className={style.radio_text}>
-                        <Field
-                          type='radio'
-                          name='position_id'
-                          value={`${position.id}`}
-                          className={style.radio_button}
-                        />
-                        {position.name}
-                      </label>
-                    </div>
-                  ))}
-              </div>
-
-              <label
-                htmlFor='file'
-                className={style.inputStyles}
-                onChange={() => textF()}
-              >
-                <span className={style.span}>
-                  <p className={style.p}>upload</p>
-                  <p className={style.p_field}>{text}</p>
-                </span>
-                <Input
-                  name='photo'
-                  type='file'
-                  id='file'
-                  className={style.feedback__file}
-                />
-              </label>
-
-              <button
-                type='submit'
-                className={buttonStyles}
-                disabled={!isValid || !dirty}
-              >
-                Sign up
-              </button>
-            </Form>
-          )
-        }}
-      </Formik> */}
       {user && user.success ? (
         <div className={style.after}>
-          <h1>User successfully registered</h1>
+          <h1 className={style.success_h1}>User successfully registered</h1>
           <img src={success} alt='success' className={style.img} />
         </div>
       ) : (
         <>
         <h1 className={style.h1}>Working with POST request</h1>
+        {error && error? (<div className={style.error_registration}  >{error}</div>):(<></>)}
         <Formik
         initialValues={initialValues}
         validationSchema={REGISTRATION_CHEMA}
